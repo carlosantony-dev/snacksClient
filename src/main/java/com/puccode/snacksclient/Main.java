bfcbbda1649253756db7028e89399823fd899c20
carlosantony-dev <carlosantony07@outlook.com>
2022-12-07 10:46:03 -0300

    hello

---
+++ src/main/java/com/puccode/snacksclient/Main.java
@@ -0,0 +1,29 @@
+package com.puccode.snacksclient;
+
+import com.puccode.snacksclient.forms.Home;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main implements Runnable {
+    JFrame jFrame;
+    static ManagedChannel channel;
+   
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Main());
+    }
+
+    @Override
+    public void run() {
+        Home home = new Home();
+        jFrame = new JFrame();
+        jFrame.setContentPane(home.getContentPane());
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setTitle("Java Snacks Restaurant");
+        jFrame.setSize(new Dimension(1080, 720));
+        jFrame.setVisible(true);
+    }
+}
